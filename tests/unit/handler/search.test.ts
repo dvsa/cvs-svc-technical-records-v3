2df9369cad8ee488b561cee8b529ba4237d3421d
Nathan Brown <nathan.brown@bjss.com>
2023-06-02 16:48:03 +0100

    feat(cb2-8084): repair tests

--- tests/unit/handler/search.test.ts
+++ tests/unit/handler/search.test.ts
@@ -15,6 +15,7 @@ jest.mock('../../../src/services/database.ts', () => ({
 }));
 
 describe('Test Search Lambda Function', () => {
+  const headers = { 'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token', 'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT', 'Access-Control-Allow-Origin': '*' };
   beforeEach(() => {
     jest.resetAllMocks();
   });
@@ -22,14 +23,14 @@ describe('Test Search Lambda Function', () => {
     it('should return an error when the query parameters are invalid', async () => {
       mockGetSearchErrors.mockReturnValueOnce({ statusCode: 400, body: 'Missing vehicle search identifier' });
       const result = await handler({ pathParameters: { foo: 'undefined' } } as unknown as APIGatewayProxyEvent);
-      expect(result).toEqual({ statusCode: 400, body: 'Missing vehicle search identifier' });
+      expect(result).toEqual({ statusCode: 400, body: 'Missing vehicle search identifier', headers });
     });
 
     it('should return a 404 if no results are found', async () => {
       mockGetSearchErrors.mockReturnValueOnce(null);
       mockSearchByAll.mockResolvedValueOnce([]);
       const result = await handler({ pathParameters: { searchIdentifier: '123456' } } as unknown as APIGatewayProxyEvent);
-      expect(result).toEqual({ statusCode: 404, body: 'No records found matching identifier 123456 and criteria all' });
+      expect(result).toEqual({ statusCode: 404, body: 'No records found matching identifier 123456 and criteria all', headers });
     });
   });
 
@@ -39,7 +40,7 @@ describe('Test Search Lambda Function', () => {
       mockSearchByCriteria.mockResolvedValueOnce(['record 1', 'record 2']);
       const result = await handler({ pathParameters: { searchIdentifier: '123456' }, queryStringParameters: { searchCriteria: 'systemNumber' } } as unknown as APIGatewayProxyEvent);
       expect(mockSearchByCriteria).toHaveBeenCalledTimes(1);
-      expect(result).toEqual({ statusCode: 200, body: '["record 1","record 2"]' });
+      expect(result).toEqual({ statusCode: 200, body: '["record 1","record 2"]', headers });
     });
 
     it('should return results when not given a search criteria', async () => {
@@ -47,7 +48,7 @@ describe('Test Search Lambda Function', () => {
       mockSearchByAll.mockResolvedValueOnce(['record 1', 'record 2']);
       const result = await handler({ pathParameters: { searchIdentifier: '123456' } } as unknown as APIGatewayProxyEvent);
       expect(mockSearchByAll).toHaveBeenCalledTimes(1);
-      expect(result).toEqual({ statusCode: 200, body: '["record 1","record 2"]' });
+      expect(result).toEqual({ statusCode: 200, body: '["record 1","record 2"]', headers });
     });
   });
 });
