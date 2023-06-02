e227f6732d425a86b8471b08424dd8d9fa95fbeb
Nathan Brown <nathan.brown@bjss.com>
2023-06-02 12:17:13 +0100

    feat(cb2-8084): add CORS headers

--- src/util/httpHeaders.ts
+++ src/util/httpHeaders.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyResult } from 'aws-lambda';
 
 export const addHttpHeaders = (httpResponse: APIGatewayProxyResult): APIGatewayProxyResult => {
   const headers = {
-    'Access-Control-Allow-Origin': "'*'",
+    'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
     'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
   };
