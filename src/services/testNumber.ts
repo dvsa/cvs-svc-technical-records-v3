77c8a44c96c92020bdbe8ac503d4c0fce35f476e
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2023-06-30 17:42:28 +0100

    api response map change

--- src/services/testNumber.ts
+++ src/services/testNumber.ts
@@ -17,9 +17,9 @@ export enum NumberTypes {
 }
 
 const apiResponseMap: Record<NumberTypes, string> = {
-  [NumberTypes.ZNumber]: 'ZNumber',
+  [NumberTypes.ZNumber]: 'zNumber',
   [NumberTypes.TrailerId]: 'trailerId',
-  [NumberTypes.TNumber]: 'TNumber',
+  [NumberTypes.TNumber]: 'tNumber',
   [NumberTypes.SystemNumber]: 'systemNumber',
 };
 
@@ -54,7 +54,10 @@ export const generateNewNumber = async (numberType: NumberTypes): Promise<string
     const response = await lambdaClient.send(command);
     const bufferResponse = Buffer.from(response.Payload!).toString('utf-8');
     const bufferBody = JSON.parse(bufferResponse).body;
+    logger.info(bufferBody);
     const apiResponseValue = apiResponseMap[numberType];
+    logger.info(apiResponseValue);
+    logger.info('returning response');
     return JSON.parse(bufferBody)[apiResponseValue];
   } catch (e) {
     logger.error(`Error in generate ${numberType} ${JSON.stringify(e)}`);
