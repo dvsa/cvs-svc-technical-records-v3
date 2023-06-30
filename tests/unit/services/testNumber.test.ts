ff5780b6263c682d9702f327bd9e794477b692c3
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2023-06-30 13:55:49 +0100

    coverage

---
+++ tests/unit/services/testNumber.test.ts
@@ -0,0 +1,35 @@
+/* eslint-disable import/first */
+import {
+  generateSystemNumber,
+  generateTNumber,
+  generateTrailerId,
+  generateZNumber,
+} from '../../../src/services/testNumber';
+
+const mockPostTechRecords = jest.fn();
+
+jest.mock('../../../src/services/database.ts', () => ({
+  postTechRecord: mockPostTechRecords,
+}));
+
+describe('Test test Number Service', () => {
+  process.env.AWS_SAM_LOCAL='true';
+  describe('Successful response', () => {
+    it('should return a system number', async () => {
+      const result = await generateSystemNumber();
+      expect(result).toBe('123');
+    });
+    it('should return a trailerId number', async () => {
+      const result = await generateTrailerId();
+      expect(result).toBe('123');
+    });
+    it('should return a Z number', async () => {
+      const result = await generateZNumber();
+      expect(result).toBe('123');
+    });
+    it('should return a T number', async () => {
+      const result = await generateTNumber();
+      expect(result).toBe('123');
+    });
+  });
+});
