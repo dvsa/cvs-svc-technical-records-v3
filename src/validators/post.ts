a73a6975ab99f77b9a52ed7a7b380f8cbc9975cc
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2023-07-12 16:03:24 +0100

    linting

--- src/validators/post.ts
+++ src/validators/post.ts
@@ -3,6 +3,5 @@ import { HttpMethod, RecordCompleteness, VehicleType } from '../util/enum';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
 export const identifyObjectType = (obj: any, method: HttpMethod) => identifySchema(obj.techRecord_vehicleType, obj.techRecord_recordCompleteness, method);
-//return one value not multiple
 export const identifySchema = (vehicleType: VehicleType, recordCompleteness: RecordCompleteness, method: HttpMethod) => schemas
   .find((x: string) => x.includes(vehicleType) && x.includes(recordCompleteness) && x.includes(method));
