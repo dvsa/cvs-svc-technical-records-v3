b2d7243c1fe24d20649682c667698455cae4a7dd
Guillaume Julien <guillaume.julien@bjss.com>
2023-07-13 13:33:09 +0100

    fix

--- src/processors/processPostRequest.ts
+++ src/processors/processPostRequest.ts
@@ -3,11 +3,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
 import { isValidObject } from '@dvsa/cvs-type-definitions/schema-validator';
-import logger from '../util/logger';
-import { generateNewNumber, NumberTypes } from '../services/testNumber';
-import { identifySchema } from '../validators/post';
-import { UserDetails } from '../services/user';
-import { HttpMethod, RecordCompleteness, VehicleType } from '../util/enum';
 import {
   TechrecordCar,
   TechrecordGet,
@@ -16,8 +11,13 @@ import {
   TechrecordPsv,
   TechrecordPut, TechrecordTrl,
 } from '../models/post';
+import { generateNewNumber, NumberTypes } from '../services/testNumber';
+import { UserDetails } from '../services/user';
+import { HttpMethod, RecordCompleteness, VehicleType } from '../util/enum';
+import logger from '../util/logger';
+import { identifySchema } from '../validators/post';
 
-export const processPostRequest = async (input: unknown, userDetails: UserDetails): TechRecordGet => {
+export const processPostRequest = async (input: unknown, userDetails: UserDetails): Promise<TechrecordGet> => {
   // we should be validating it's a valid technical record HERE.)
   if (isObjectEmpty(input)) {
     throw new Error('Invalid Technical Record');
@@ -27,17 +27,21 @@ export const processPostRequest = async (input: unknown, userDetails: UserDetail
   (request as TechrecordGet).techRecord_recordCompleteness = computeRecordCompleteness(request);
   // helper method for handler
   const systemNumber = await generateNewNumber(NumberTypes.SystemNumber);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   if (request.techRecord_vehicleType !== 'trl' && !request.primaryVrm) {
-    (request as (TechrecordPsv | TechrecordCar | TechrecordHgv | TechrecordMotorcycle)).primaryVrm = await generateNewNumber(NumberTypes.ZNumber);
+    (request as TechrecordHgv | TechrecordMotorcycle | TechrecordCar).primaryVrm = await generateNewNumber(NumberTypes.ZNumber);
   }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   if (request.techRecord_vehicleType === 'trl' && !request.trailerId && (request.techRecord_euVehicleCategory === 'o1' || request.techRecord_euVehicleCategory === 'o2')) {
     (request as TechrecordTrl).trailerId = await generateNewNumber(NumberTypes.TNumber);
   }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   if (request.techRecord_vehicleType === 'trl' && !request.trailerId) {
     (request as TechrecordTrl).trailerId = await generateNewNumber(NumberTypes.TrailerId);
   }
   (request as TechrecordGet).systemNumber = systemNumber;
   (request as TechrecordGet).createdTimestamp = new Date().toISOString();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
   (request as TechrecordGet).partialVin = request.vin.length < 6 ? request.vin : request.vin.substring(request.vin.length - 6);
   (request as TechrecordGet).techRecord_createdByName = userDetails.username;
   (request as TechrecordGet).techRecord_createdById = userDetails.msOid;
@@ -51,12 +55,15 @@ export function computeRecordCompleteness(input: TechrecordPut): RecordCompleten
     logger.info('general errors: ', generalVehicleErrors);
     return RecordCompleteness.SKELETON;
   }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   const isCompleteSchema = identifySchema(input.techRecord_vehicleType as VehicleType, RecordCompleteness.COMPLETE, HttpMethod.PUT);
   let isComplete;
   if (isCompleteSchema) {
     isComplete = isValidObject(isCompleteSchema, input);
   }
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   const isTestableSchema = input.techRecord_vehicleType === (VehicleType.TRL || VehicleType.PSV || VehicleType.HGV)
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     ? identifySchema(input.techRecord_vehicleType as VehicleType, RecordCompleteness.TESTABLE, HttpMethod.PUT) : '';
   let isTestable = false;
   if (isTestableSchema) {
@@ -74,10 +81,11 @@ export function computeRecordCompleteness(input: TechrecordPut): RecordCompleten
 }
 
 const generalErrors = (input: TechrecordPut) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   if (!input.techRecord_vehicleType) {
     return 'Missing vehicle type';
   }
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unnecessary-type-assertion
   if ((input as any).techRecord_hiddenInVta) {
     return 'skeleton';
   }
