37ab1dff713d20cc0b5b0981f28349ba3a310837
Nathan Brown <nathan.brown@bjss.com>
2023-07-26 14:33:54 +0100

    feat(cb2-8166): repair try location

--- src/handler/promote.ts
+++ src/handler/promote.ts
@@ -14,77 +14,77 @@ import { validatePromoteErrors } from '../validators/promote';
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   logger.info('Promote end point called');
 
-  const promoteErrors = validatePromoteErrors(event);
-
-  if (promoteErrors) {
-    return addHttpHeaders(promoteErrors);
-  }
-
-  const systemNumber = decodeURIComponent(event?.pathParameters?.systemNumber ?? '');
-  const createdTimestamp = decodeURIComponent(event?.pathParameters?.createdTimestamp ?? '');
-  const userDetails = getUserDetails(event.headers.Authorization ?? '');
-  const { reasonForPromoting } = JSON.parse(event.body as string) as PromoteRecordRequestBody;
-
-  logger.info(`Get from database with systemNumber ${systemNumber} and timestamp ${createdTimestamp}`);
-
-  const provisionalRecord: TechrecordGet = await getBySystemNumberAndCreatedTimestamp(systemNumber, createdTimestamp) as TechrecordGet;
-
-  logger.debug(`result is: ${JSON.stringify(provisionalRecord)}`);
-
-  if (!provisionalRecord || !Object.keys(provisionalRecord).length) {
-    return addHttpHeaders({
-      statusCode: 404,
-      body: `No record found matching systemNumber ${systemNumber} and timestamp ${createdTimestamp}`,
-    });
-  }
-
-  if (provisionalRecord.techRecord_statusCode !== StatusCode.PROVISIONAL) {
-    return addHttpHeaders({
-      statusCode: 400,
-      body: 'Record provided is not a provisional record so cannot be promoted.',
-    });
-  }
-
-  const allVehicleRecords = await searchByCriteria(SearchCriteria.SYSTEM_NUMBER, provisionalRecord.systemNumber);
-  const currentResult = allVehicleRecords.filter((searchResult) => searchResult.techRecord_statusCode === StatusCode.CURRENT);
-
-  logger.debug(`Current result ${JSON.stringify(currentResult)}`);
-
-  const recordsToArchive: TechrecordGet[] = [];
-
-  if (currentResult.length > 0) {
-    const currentRecord = await getBySystemNumberAndCreatedTimestamp(currentResult[0].systemNumber, currentResult[0].createdTimestamp) as TechrecordGet;
-    currentRecord.techRecord_statusCode = StatusCode.ARCHIVED;
-    currentRecord.techRecord_lastUpdatedAt = new Date().toISOString();
-    currentRecord.techRecord_lastUpdatedByName = userDetails.username;
-    currentRecord.techRecord_lastUpdatedById = userDetails.msOid;
-    recordsToArchive.push(currentRecord);
-
-    logger.debug(`Old current record after update ${JSON.stringify(currentRecord)}`);
-  }
-
-  const newCurrentRecord = cloneDeep(provisionalRecord);
-
-  newCurrentRecord.techRecord_statusCode = StatusCode.CURRENT;
-  newCurrentRecord.createdTimestamp = new Date().toISOString();
-  delete newCurrentRecord.techRecord_lastUpdatedAt;
-  newCurrentRecord.techRecord_createdByName = userDetails.username;
-  newCurrentRecord.techRecord_createdById = userDetails.msOid;
-  newCurrentRecord.techRecord_reasonForCreation = reasonForPromoting;
-
-  logger.debug(`New current record after update ${JSON.stringify(newCurrentRecord)}`);
-
-  provisionalRecord.techRecord_statusCode = StatusCode.ARCHIVED;
-  provisionalRecord.techRecord_lastUpdatedAt = new Date().toISOString();
-  provisionalRecord.techRecord_lastUpdatedByName = userDetails.username;
-  provisionalRecord.techRecord_lastUpdatedById = userDetails.msOid;
-  recordsToArchive.push(provisionalRecord);
-
-  logger.debug(`Old provisional record after update ${JSON.stringify(provisionalRecord)}`);
-
-  await updateVehicle(recordsToArchive, newCurrentRecord);
-
   try {
+    const promoteErrors = validatePromoteErrors(event);
+
+    if (promoteErrors) {
+      return addHttpHeaders(promoteErrors);
+    }
+
+    const systemNumber = decodeURIComponent(event?.pathParameters?.systemNumber ?? '');
+    const createdTimestamp = decodeURIComponent(event?.pathParameters?.createdTimestamp ?? '');
+    const userDetails = getUserDetails(event.headers.Authorization ?? '');
+    const { reasonForPromoting } = JSON.parse(event.body as string) as PromoteRecordRequestBody;
+
+    logger.info(`Get from database with systemNumber ${systemNumber} and timestamp ${createdTimestamp}`);
+
+    const provisionalRecord: TechrecordGet = await getBySystemNumberAndCreatedTimestamp(systemNumber, createdTimestamp) as TechrecordGet;
+
+    logger.debug(`result is: ${JSON.stringify(provisionalRecord)}`);
+
+    if (!provisionalRecord || !Object.keys(provisionalRecord).length) {
+      return addHttpHeaders({
+        statusCode: 404,
+        body: `No record found matching systemNumber ${systemNumber} and timestamp ${createdTimestamp}`,
+      });
+    }
+
+    if (provisionalRecord.techRecord_statusCode !== StatusCode.PROVISIONAL) {
+      return addHttpHeaders({
+        statusCode: 400,
+        body: 'Record provided is not a provisional record so cannot be promoted.',
+      });
+    }
+
+    const allVehicleRecords = await searchByCriteria(SearchCriteria.SYSTEM_NUMBER, provisionalRecord.systemNumber);
+    const currentResult = allVehicleRecords.filter((searchResult) => searchResult.techRecord_statusCode === StatusCode.CURRENT);
+
+    logger.debug(`Current result ${JSON.stringify(currentResult)}`);
+
+    const recordsToArchive: TechrecordGet[] = [];
+
+    if (currentResult.length > 0) {
+      const currentRecord = await getBySystemNumberAndCreatedTimestamp(currentResult[0].systemNumber, currentResult[0].createdTimestamp) as TechrecordGet;
+      currentRecord.techRecord_statusCode = StatusCode.ARCHIVED;
+      currentRecord.techRecord_lastUpdatedAt = new Date().toISOString();
+      currentRecord.techRecord_lastUpdatedByName = userDetails.username;
+      currentRecord.techRecord_lastUpdatedById = userDetails.msOid;
+      recordsToArchive.push(currentRecord);
+
+      logger.debug(`Old current record after update ${JSON.stringify(currentRecord)}`);
+    }
+
+    const newCurrentRecord = cloneDeep(provisionalRecord);
+
+    newCurrentRecord.techRecord_statusCode = StatusCode.CURRENT;
+    newCurrentRecord.createdTimestamp = new Date().toISOString();
+    delete newCurrentRecord.techRecord_lastUpdatedAt;
+    newCurrentRecord.techRecord_createdByName = userDetails.username;
+    newCurrentRecord.techRecord_createdById = userDetails.msOid;
+    newCurrentRecord.techRecord_reasonForCreation = reasonForPromoting;
+
+    logger.debug(`New current record after update ${JSON.stringify(newCurrentRecord)}`);
+
+    provisionalRecord.techRecord_statusCode = StatusCode.ARCHIVED;
+    provisionalRecord.techRecord_lastUpdatedAt = new Date().toISOString();
+    provisionalRecord.techRecord_lastUpdatedByName = userDetails.username;
+    provisionalRecord.techRecord_lastUpdatedById = userDetails.msOid;
+    recordsToArchive.push(provisionalRecord);
+
+    logger.debug(`Old provisional record after update ${JSON.stringify(provisionalRecord)}`);
+
+    await updateVehicle(recordsToArchive, newCurrentRecord);
+
     return addHttpHeaders({
       statusCode: 200,
       body: JSON.stringify(newCurrentRecord),
