47cae4ed212abc17635e70342898d47406b2432e
Nathan Brown <nathan.brown@bjss.com>
2023-07-28 15:46:35 +0100

    refactor vrm too

--- src/processors/processVrmRequest.ts
+++ src/processors/processVrmRequest.ts
@@ -1,22 +1,17 @@
 import {
   TechRecordCar, TechRecordGet, TechRecordHgv, TechRecordMotorcycle, TechRecordPsv,
 } from '../models/post';
+import { setCreatedAuditDetails, setLastUpdatedAuditDetails } from '../services/audit';
 import { UserDetails } from '../services/user';
+import { StatusCode } from '../util/enum';
 
 export const processPatchVrmRequest = (currentRecord: TechRecordGet, userDetails: UserDetails, newVrm: string): Array<TechRecordGet> => {
   const recordToArchive: TechRecordGet = { ...currentRecord };
   const newRecord: TechRecordGet = { ...currentRecord };
-  const date: string = new Date().toISOString();
 
-  (newRecord as TechRecordHgv | TechRecordMotorcycle | TechRecordCar | TechRecordPsv).primaryVrm = newVrm.toUpperCase();
-  newRecord.createdTimestamp = date;
-  delete newRecord.techRecord_lastUpdatedAt;
-  newRecord.techRecord_createdByName = userDetails.username;
-  newRecord.techRecord_createdById = userDetails.msOid;
-  recordToArchive.techRecord_statusCode = 'archived';
-  recordToArchive.techRecord_lastUpdatedAt = date;
-  recordToArchive.techRecord_lastUpdatedByName = userDetails.username;
-  recordToArchive.techRecord_lastUpdatedById = userDetails.msOid;
+  const updatedNewRecord = setCreatedAuditDetails(newRecord, userDetails.username, userDetails.msOid, new Date().toISOString(), currentRecord.techRecord_statusCode as StatusCode);
+  (updatedNewRecord as TechRecordHgv | TechRecordMotorcycle | TechRecordCar | TechRecordPsv).primaryVrm = newVrm.toUpperCase();
+  const updatedRecordToArchive = setLastUpdatedAuditDetails(recordToArchive, userDetails.username, userDetails.msOid, new Date().toISOString(), StatusCode.ARCHIVED);
 
-  return [recordToArchive, newRecord];
+  return [updatedRecordToArchive, newRecord];
 };
