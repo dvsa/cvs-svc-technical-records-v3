23765ae2cc6269e04418fcf8d5d2ad249748ae44
Nathan Brown <nathan.brown@bjss.com>
2023-08-23 16:37:24 +0100

    chore(*): update bad enums

--- src/models/letter.ts
+++ src/models/letter.ts
@@ -13,14 +13,14 @@ export interface Letter {
 }
 
 export enum LetterType {
-  TRL_ACCEPTANCE = 'trailer accept',
+  TRL_ACCEPTANCE = 'trailer acceptance',
   TRL_REJECTION = 'trailer rejection',
 }
 
 export enum ParagraphId {
-  PARAGRAPH_3 = '3',
-  PARAGRAPH_4 = '4',
-  PARAGRAPH_5 = '5',
-  PARAGRAPH_6 = '6',
-  PARAGRAPH_7 = '7',
+  PARAGRAPH_3 = 3,
+  PARAGRAPH_4 = 4,
+  PARAGRAPH_5 = 5,
+  PARAGRAPH_6 = 6,
+  PARAGRAPH_7 = 7,
 }
