be81e49b6892354cb211a94c373b16a31bc87052
Guillaume Julien <guillaume.julien@bjss.com>
2023-08-29 11:40:06 +0100

    Revert "bump package version"
    This reverts commit 7b7d829f3c22bdf433dd27f1479f13f2db58223d.

--- src/processors/processPostRequest.ts
+++ src/processors/processPostRequest.ts
@@ -14,6 +14,9 @@ export const processPostRequest = async (input: TechRecordType<'put'>, userDetai
     throw new Error(ERRORS.MISSING_PAYLOAD);
   }
 
+  input.techRecord_createdByName = userDetails.username;
+  input.techRecord_createdById = userDetails.msOid;
+
   (input as TechRecordType<'get'>).techRecord_recordCompleteness = validateAndComputeRecordCompleteness(input, HttpMethod.PUT);
 
   const requestAsGet: TechRecordType<'get'> = flattenArrays(input) as TechRecordType<'get'>;
@@ -28,12 +31,8 @@ export const processPostRequest = async (input: TechRecordType<'put'>, userDetai
   if (isTRL(requestAsGet) && !requestAsGet.trailerId) {
     requestAsGet.trailerId = await generateNewNumber(NumberTypes.TrailerId);
   }
-  const now = new Date().toISOString();
-  requestAsGet.techRecord_createdByName = userDetails.username;
-  requestAsGet.techRecord_createdById = userDetails.msOid;
   requestAsGet.systemNumber = systemNumber;
-  requestAsGet.createdTimestamp = now;
-  requestAsGet.techRecord_createdAt = now;
+  requestAsGet.createdTimestamp = new Date().toISOString();
   requestAsGet.partialVin = requestAsGet.vin.length < 6 ? requestAsGet.vin : requestAsGet.vin.substring(requestAsGet.vin.length - 6);
   logger.info('Successfully Processed Record');
   return requestAsGet;
