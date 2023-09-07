a2f58ebee1f2cf7005dfa16e7e1a382b0416d955
owen <owen.corrigan@bjss.com>
2023-09-07 19:25:57 +0100

    feat(cb2-8710): add try catch and return the record

--- src/processors/processCorrectVrm.ts
+++ src/processors/processCorrectVrm.ts
@@ -13,28 +13,35 @@ export const processCorrectVrm = async (
   userDetails: UserDetails,
   newVrm: string,
 ): Promise<APIGatewayProxyResult> => {
-  const techRecords = await searchByCriteria(SearchCriteria.PRIMARYVRM, newVrm);
-  const filteredVrm = techRecords.filter((x) => x.primaryVrm === newVrm && x.techRecord_statusCode !== StatusCode.ARCHIVED);
-  if (filteredVrm.length) {
+  try {
+    const techRecords = await searchByCriteria(SearchCriteria.PRIMARYVRM, newVrm);
+    const filteredVrm = techRecords.filter((x) => x.primaryVrm === newVrm && x.techRecord_statusCode !== StatusCode.ARCHIVED);
+    if (filteredVrm.length) {
+      return addHttpHeaders({
+        statusCode: 400,
+        body: JSON.stringify(`Primary VRM ${newVrm} already exists`),
+      });
+    }
+    logger.debug('identifier has been validated');
+
+    const newRecord: TechRecordType<'get'> = { ...currentRecord };
+    if (newRecord.techRecord_vehicleType !== 'trl') {
+      newRecord.primaryVrm = newVrm.toUpperCase();
+    }
+    newRecord.techRecord_lastUpdatedAt = new Date().toISOString();
+    newRecord.techRecord_lastUpdatedById = userDetails.msOid;
+    newRecord.techRecord_lastUpdatedByName = userDetails.username;
+
+    await correctVrm(newRecord);
+
     return addHttpHeaders({
-      statusCode: 400,
-      body: JSON.stringify(`Primary VRM ${newVrm} already exists`),
+      statusCode: 200,
+      body: JSON.stringify(formatTechRecord(newRecord)),
+    });
+  } catch (err) {
+    return addHttpHeaders({
+      statusCode: 500,
+      body: JSON.stringify(err),
     });
   }
-  logger.debug('identifier has been validated');
-
-  const newRecord: TechRecordType<'get'> = { ...currentRecord };
-  if (newRecord.techRecord_vehicleType !== 'trl') {
-    newRecord.primaryVrm = newVrm.toUpperCase();
-  }
-  newRecord.techRecord_lastUpdatedAt = new Date().toISOString();
-  newRecord.techRecord_lastUpdatedById = userDetails.msOid;
-  newRecord.techRecord_lastUpdatedByName = userDetails.username;
-
-  const correctedRecord = await correctVrm(newRecord);
-
-  return addHttpHeaders({
-    statusCode: 200,
-    body: JSON.stringify(formatTechRecord(correctedRecord)),
-  });
 };
