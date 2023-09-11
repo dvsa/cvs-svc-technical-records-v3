77ceae90493700ad59b7c9f1bf04bb8a35cebaae
owen <owen.corrigan@bjss.com>
2023-09-11 15:38:38 +0100

    feat(cb2-8710): console log

--- src/handler/updateVrm.ts
+++ src/handler/updateVrm.ts
@@ -52,6 +52,7 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
         newDonorVrm,
         donorVehicleRecord,
       );
+      console.log('hiya');
 
       await updateVehicle([recipientRecordToArchive, donorRecordToArchive], [newRecipientRecord, newDonorRecord]);
 
