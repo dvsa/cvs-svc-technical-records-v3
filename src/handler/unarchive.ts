2f47abd5e6e4db2a6f8feaa3cd97499595e530ac
kristian carr <kristian.carr@bjss.com>
2023-09-14 16:58:33 +0100

    (feat/cb2-8225): tidied up unarchive handler

--- src/handler/unarchive.ts
+++ src/handler/unarchive.ts
@@ -30,57 +30,36 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
 
     logger.info(`Get from database with systemNumber ${systemNumber} and timestamp ${createdTimestamp}`);
 
-    const archivedRecord: TechRecordType<'get'> = await getBySystemNumberAndCreatedTimestamp(systemNumber, createdTimestamp);
-    logger.debug(`result is: ${JSON.stringify(archivedRecord)}`);
+    const record: TechRecordType<'get'> = await getBySystemNumberAndCreatedTimestamp(systemNumber, createdTimestamp);
+    logger.debug(`result is: ${JSON.stringify(record)}`);
 
-    if (!archivedRecord || !Object.keys(archivedRecord).length) {
-      return addHttpHeaders({
-        statusCode: 404,
-        body: `No record found matching systemNumber ${systemNumber} and timestamp ${createdTimestamp}`,
-      });
+    if (!record || !Object.keys(record).length) {
+      return addHttpHeaders({ statusCode: 404, body: `No record found matching systemNumber ${systemNumber} and timestamp ${createdTimestamp}`});
     }
 
-    if (archivedRecord.techRecord_statusCode !== StatusCode.ARCHIVED) {
-      return addHttpHeaders({
-        statusCode: 400,
-        body: 'Record provided is not an archived record so cannot be unarchived.',
-      });
+    if (record.techRecord_statusCode !== StatusCode.ARCHIVED) {
+      return addHttpHeaders({ statusCode: 400, body: 'Record provided is not an archived record so cannot be unarchived.'});
     }
 
-    const allVehicleRecords = await searchByCriteria(SearchCriteria.SYSTEM_NUMBER, archivedRecord.systemNumber);
-    const hasUnarchivedRecords: boolean = allVehicleRecords.some((searchResult) => searchResult.techRecord_statusCode !== StatusCode.ARCHIVED
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-      && searchResult.primaryVrm === (archivedRecord as unknown as any).primaryVrm);
+    const { primaryVrm } = (record as { primaryVrm?: string });
+    const hasUnarchivedRecords = (await searchByCriteria(SearchCriteria.SYSTEM_NUMBER, record.systemNumber)).some((searchResult) => searchResult.techRecord_statusCode !== StatusCode.ARCHIVED && searchResult.primaryVrm === primaryVrm);
 
     if (hasUnarchivedRecords) {
-      return addHttpHeaders({
-        statusCode: 400,
-        body: 'Cannot archive a record with unarchived records',
-      });
+      return addHttpHeaders({ statusCode: 400, body: 'Cannot archive a record with unarchived records'});
     }
 
-    let recordToCreate: TechRecordType<'get'> = cloneDeep(archivedRecord);
-    recordToCreate.techRecord_reasonForCreation = reasonForUnarchiving;
-    recordToCreate = setCreatedAuditDetails(
-      recordToCreate,
-      userDetails.username,
-      userDetails.msOid,
-      new Date().toISOString(),
-      status as StatusCode,
-    );
+    const recordToCreate: TechRecordType<'get'> = {
+      ...cloneDeep(record),
+      techRecord_reasonForCreation: reasonForUnarchiving,
+    };
+
+    setCreatedAuditDetails(recordToCreate, userDetails.username, userDetails?.msOid, new Date().toISOString(), status as StatusCode);
 
     const postResponse = await postTechRecord(recordToCreate);
 
-    return addHttpHeaders({
-      statusCode: 200,
-      body: JSON.stringify(formatTechRecord(postResponse)),
-    });
+    return addHttpHeaders({ statusCode: 200, body: JSON.stringify(formatTechRecord(postResponse)) });
   } catch (error) {
     logger.error(`Error has been thrown with ${JSON.stringify(error)}`);
-    return addHttpHeaders({
-      statusCode: 500,
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      body: JSON.stringify({ error: `Failed to unarchive record: ${error}` }),
-    });
+    return addHttpHeaders({ statusCode: 500, body: JSON.stringify({ error: `Failed to unarchive record: ${error}` })});
   }
 };
