259b5b85dda9e11358257645b58f114a6efdd3dc
Kristian Carr <127743278+kcarr-bjss@users.noreply.github.com>
2023-09-15 09:48:05 +0100

    feat(cb2-8225): Unarchive Technical Record (#59)

---
+++ src/validators/unarchive.ts
@@ -0,0 +1,47 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { validateSysNumTimestampPathParams } from './sysNumTimestamp';
+import { UnarchiveRequestBody } from '../models/unarchive';
+import { StatusCode } from '../util/enum';
+
+export const validateUnarchiveErrors = (event: APIGatewayProxyEvent): APIGatewayProxyResult | undefined => {
+  if (!event.headers.Authorization) {
+    return {
+      statusCode: 400,
+      body: 'Missing authorization header',
+    };
+  }
+
+  const isPathInvalid: APIGatewayProxyResult | undefined = validateSysNumTimestampPathParams(event);
+
+  if (isPathInvalid) {
+    return isPathInvalid;
+  }
+
+  if (!event.body || !Object.keys(event.body).length) {
+    return {
+      statusCode: 400,
+      body: 'invalid request',
+    };
+  }
+
+  const body: UnarchiveRequestBody = JSON.parse(event.body) as UnarchiveRequestBody;
+
+  if (!body.reasonForUnarchiving) {
+    return {
+      statusCode: 400,
+      body: 'Reason for unarchiving not provided',
+    };
+  }
+
+  const validStatusCodes = [StatusCode.CURRENT, StatusCode.PROVISIONAL];
+  const statusCodeIsValid = validStatusCodes.some((statusCode) => statusCode === body?.status);
+
+  if (!statusCodeIsValid) {
+    return {
+      statusCode: 400,
+      body: 'Invalid status provided',
+    };
+  }
+
+  return undefined;
+};
