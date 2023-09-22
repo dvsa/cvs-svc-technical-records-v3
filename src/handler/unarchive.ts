4318cc90e3009ac32bdccfeaba118a24be5cbfa1
Kristian Carr <127743278+kcarr-bjss@users.noreply.github.com>
2023-09-22 10:34:35 +0100

    fix(cb2 8225): Unarchive technical records with different VRMs (#64)

--- src/handler/unarchive.ts
+++ src/handler/unarchive.ts
@@ -42,8 +42,10 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
     }
 
     const { primaryVrm } = (record as { primaryVrm?: string });
-    const hasUnarchivedRecords = (await searchByCriteria(SearchCriteria.SYSTEM_NUMBER, record.systemNumber))
-      .some((searchResult) => searchResult.techRecord_statusCode !== StatusCode.ARCHIVED && searchResult.primaryVrm === primaryVrm);
+    const hasUnarchivedRecords = (await searchByCriteria(SearchCriteria.PRIMARYVRM, primaryVrm as string))
+      .some((searchResult) => searchResult.techRecord_statusCode !== StatusCode.ARCHIVED
+        && searchResult.techRecord_vehicleType !== 'trl'
+        && searchResult.primaryVrm === primaryVrm);
 
     if (hasUnarchivedRecords) {
       return addHttpHeaders({ statusCode: 400, body: 'Cannot archive a record with unarchived records' });
