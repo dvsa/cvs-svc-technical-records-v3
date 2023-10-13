b21ea4520cadfc77b753e7273348b4f1e63f8cf2
Andrew Freemantle (BJSS) <andrew.freemantle@bjss.com>
2023-10-13 15:18:43 +0100

    Revert "fix(cb2-7464): plate validation (#69)"
    This reverts commit 3b8c462d19b549101b6d0a3737fd51d29bcfaa5c.

--- src/handler/plate.ts
+++ src/handler/plate.ts
@@ -119,16 +119,9 @@ function validateTechRecordPlates(record: HgvOrTrl): APIGatewayProxyResult | und
 }
 
 function cannotGeneratePlate(plateRequiredFields: string[], record: HgvOrTrl): boolean {
-  const isOneFieldEmpty = plateRequiredFields.some((field) => {
-    const value = record[field as keyof HgvOrTrl];
-    return value === undefined || value === null || value === '';
-  });
+  const isOneFieldEmpty = plateRequiredFields.some((field) => !record[field as keyof HgvOrTrl]);
   const areAxlesInvalid = record.techRecord_axles?.some((axle) => axleRequiredFields.some(
-    (field) => {
-      const value = (axle as HGVAxles)[field as keyof HGVAxles];
-      return value === undefined || value === null || value === '';
-    },
-
+    (field) => !(axle as HGVAxles)[field as keyof HGVAxles],
   ));
 
   return isOneFieldEmpty || !record.techRecord_axles?.length || !!areAxlesInvalid;
