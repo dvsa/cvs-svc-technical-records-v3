263cfbef3a7410a94ebb2bc986b1fe3ca719568d
kristian carr <kristian.carr@bjss.com>
2024-02-23 11:39:39 +0000

    feat(cb2-10905): added bash script to replicate webpack functionality

---
+++ scripts/package.sh
@@ -0,0 +1,11 @@
+directory="/.aws-sam/build/*"
+outputFolder="./";
+commitHash="${ZIP_NAME:-local}"
+
+cd .aws-sam/build
+
+for lambdaName in */; do
+  formattedLambdaName=`basename ${lambdaName} /`
+  cp -r "../../node_modules/@dvsa/cvs-type-definitions/json-schemas/" "${formattedLambdaName}/json-schemas"
+  zip -qr ../../${commitHash}-${formattedLambdaName}.zip ${formattedLambdaName}
+done
