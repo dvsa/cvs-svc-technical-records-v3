bee4e1e51c987303960936e12155039c824a280c
kristian carr <kristian.carr@bjss.com>
2024-02-24 07:53:13 +0000

    feat(cb2-10905): corrected structure of zip files generated by packaging process

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,9 +1,13 @@
 commitHash="${ZIP_NAME:-local}"
 
-cd .aws-sam/build
+pushd .aws-sam/build
 
 for lambdaName in */; do
   formattedLambdaName=`basename ${lambdaName} /`
   cp -r "../../node_modules/@dvsa/cvs-type-definitions/json-schemas/" "${formattedLambdaName}/json-schemas"
-  zip -qr ../../${commitHash}-${formattedLambdaName}.zip ${formattedLambdaName}
+  pushd $formattedLambdaName
+  zip -qr ../../../${commitHash}-${formattedLambdaName}.zip .
+  popd
 done
+
+popd
