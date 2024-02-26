32a2d5de2d091c138e9753377ab808cb7122833a
kristian carr <kristian.carr@bjss.com>
2024-02-26 14:32:07 +0000

    feat(cb2-10905): converted .js files to .mjs files and renamed resulting artifacts to app

--- scripts/package.sh
+++ scripts/package.sh
@@ -5,7 +5,17 @@ pushd .aws-sam/build
 for lambdaName in */; do
   formattedLambdaName=`basename ${lambdaName} /`
   cp -r "../../node_modules/@dvsa/cvs-type-definitions/json-schemas/" "${formattedLambdaName}/json-schemas"
+  cp -r "../../package.json" "${formattedLambdaName}"
   pushd $formattedLambdaName
+
+  for file in *.mjs; do
+    mv -- "$file" "app.mjs"
+  done
+
+  for file in *.mjs.map; do
+    mv -- "$file" "app.mjs.map"
+  done
+
   zip -qr ../../../${commitHash}-${formattedLambdaName}.zip .
   popd
 done
