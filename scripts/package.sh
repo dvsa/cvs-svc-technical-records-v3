1dad1218f64f44765b88855869965ec6b87ca8d0
kristian carr <kristian.carr@bjss.com>
2024-02-28 11:46:46 +0000

    feat(cb2-10905): fixed nesting issue with generated zip files

--- scripts/package.sh
+++ scripts/package.sh
@@ -7,5 +7,7 @@ for lambdaName in */; do
   mv ${formattedLambdaName}/*.js ${formattedLambdaName}/app.js
   mv ${formattedLambdaName}/*.js.map ${formattedLambdaName}/app.js.map
   cp -r "../../node_modules/@dvsa/cvs-type-definitions/json-schemas/" "${formattedLambdaName}/json-schemas"
-  zip -qrj ../../${commitHash}-${formattedLambdaName}.zip ${formattedLambdaName}
+  pushd $formattedLambdaName
+  zip -qr ../../../${commitHash}-${formattedLambdaName}.zip .
+  popd
 done
