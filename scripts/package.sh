05eba78dfce392bd7f9c246c293cc0a4ff1ab6ce
Matt Waldron <matt.waldron@bjss.com>
2024-02-28 09:29:15 +0000

    move build output to app

--- scripts/package.sh
+++ scripts/package.sh
@@ -4,6 +4,8 @@ cd .aws-sam/build
 
 for lambdaName in */; do
   formattedLambdaName=`basename ${lambdaName} /`
+  mv ${formattedLambdaName}/*.js ${formattedLambdaName}/app.js
+  mv ${formattedLambdaName}/*.js.map ${formattedLambdaName}/app.js.map
   cp -r "../../node_modules/@dvsa/cvs-type-definitions/json-schemas/" "${formattedLambdaName}/json-schemas"
-  zip -qr ../../${commitHash}-${formattedLambdaName}.zip ${formattedLambdaName}
+  zip -qrj ../../${commitHash}-${formattedLambdaName}.zip ${formattedLambdaName}
 done
