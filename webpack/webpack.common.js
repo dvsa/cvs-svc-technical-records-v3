386e1141f2bd1c4ad1419528e1484ac126793c15
Matt Waldron <matt.waldron@bjss.com>
2024-02-29 10:37:06 +0000

    fix(cb2-10905): depend on sdk in the container

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -23,13 +23,10 @@ module.exports = {
   // Target node
   target: 'node',
 
-  // AWS recommends always including the aws-sdk in your Lambda package but excluding can significantly reduce
-  // the size of your deployment package. If you want to always include it then comment out this line. It has
-  // been included conditionally because the node10.x docker image used by SAM local doesn't include it.
-  // externals: process.env.NODE_ENV === 'development' ? [] : ['aws-sdk'],
-  externals: {
-    fsevents: 'require(\'fsevents\')',
-  },
+  externals: [
+    /aws-sdk/,
+    'fsevents'
+  ],
 
   // Add the TypeScript loader
   module: {
