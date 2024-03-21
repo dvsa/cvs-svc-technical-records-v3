7c9e15df793d9eb6cd0b4c8fd4b6a700af15d25a
Matt Waldron <12972009+me-matt@users.noreply.github.com>
2024-03-21 12:31:09 +0000

    feat(cb2-11450): allow system user to be forced for the user context (#134)

--- tests/unit/services/users.unit.test.ts
+++ tests/unit/services/users.unit.test.ts
@@ -18,4 +18,32 @@ describe('Test User Service', () => {
       expect(() => getUserDetails(`${header}.${payload}.${signature}`)).toThrow(ERRORS.MISSING_USER_DETAILS);
     });
   });
+
+  describe('Should override with system user when the environment variable is set', () => {
+    beforeEach(() => {
+      process.env.ENABLE_SYSTEM_USER_IMPERSONATION = undefined;
+    });
+
+    it('should successfully get the system user', () => {
+      process.env.ENABLE_SYSTEM_USER_IMPERSONATION = 'true';
+
+      const header = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+      const payload = 'eyJvaWQiOjE1MTYyMzkwMjJ9';
+      const signature = 'OeYj2GlIUPh1y-xb6UMvq5m8V_nPFX5D_sBA4Fcnmz8';
+
+      const res : UserDetails = getUserDetails(`${header}.${payload}.${signature}`);
+
+      expect(res.username).toBe('SYSTEM_USER');
+      expect(res.email).toBe('SYSTEM_USER');
+    });
+
+    it('should throw an error if the environment variable is not set', () => {
+      console.log('process.env.ENABLE_SYSTEM_USER_IMPERSONATION', process.env.ENABLE_SYSTEM_USER_IMPERSONATION);
+      const header = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
+      const payload = 'eyJvaWQiOjE1MTYyMzkwMjJ9';
+      const signature = 'OeYj2GlIUPh1y-xb6UMvq5m8V_nPFX5D_sBA4Fcnmz8';
+
+      expect(() => getUserDetails(`${header}.${payload}.${signature}`)).toThrow(ERRORS.MISSING_USER_DETAILS);
+    });
+  });
 });
