fd1f355b7bced1a7b0a0e1134d7df3facabb86fa
kristian carr <kristian.carr@bjss.com>
2024-04-29 10:33:55 +0100

    feat(cb2-11360): refactored mot cherished transfer to work with SQSEvents

--- src/models/motCherishedTransfer.ts
+++ src/models/motCherishedTransfer.ts
@@ -2,8 +2,3 @@ export interface MotCherishedTransfer {
   vin: string;
   vrm: string;
 }
-
-export interface ProcessedMotCherishedTransfers {
-  fileName: string;
-  cherishedTransfers: Array<MotCherishedTransfer>;
-}
