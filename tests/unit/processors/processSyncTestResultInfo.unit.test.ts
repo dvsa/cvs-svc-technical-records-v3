59df3ee14000ac5a4fb81f1e28a756598e4f724b
Matthew Bell <33056264+matthew2564@users.noreply.github.com>
2026-05-11 14:59:27 +0100

    feat: ignore linting

--- tests/unit/processors/processSyncTestResultInfo.unit.test.ts
+++ tests/unit/processors/processSyncTestResultInfo.unit.test.ts
@@ -78,8 +78,10 @@ describe('syncTestResultInfo', () => {
       mockUpdateVehicle.mockResolvedValueOnce({});
       await syncTestResultInfo('5000', 'submitted', 'pass', '10', '012345', 'Test User', EUVehicleCategory.M2);
 
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       const [, newRecords] = mockUpdateVehicle.mock.calls[0];
       expect(newRecords).toHaveLength(2);
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       expect(newRecords[0].createdTimestamp).not.toEqual(newRecords[1].createdTimestamp);
     });
     it('should not call update if record is current and EuVehicleCategory is not updated', async () => {
